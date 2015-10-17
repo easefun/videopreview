b462e71c1e42e5250c2b5852dfc049c7018011f6
appwallets <junjing@gmail.com>
2015-10-18 00:00:32 +0800

    Initial Commit

---
+++ videopreview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  videopreview
+//
+//  Created by seanwong on 10/18/15.
+//  Copyright © 2015 easefun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ videopreview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  videopreview
+//
+//  Created by seanwong on 10/18/15.
+//  Copyright © 2015 easefun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
